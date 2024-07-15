377a817e40f71318df6ca98377e4c86a6aaba17c
Haylie Grayson <95108145+aaringrayson@users.noreply.github.com>
2024-07-15 18:09:16 -0400

    Flatirons Land Co

---
+++ main.js
@@ -0,0 +1,6 @@
+const accordions = document.querySelectorAll('.accordion');
+accordions.forEach(accordion => {
+    accordion.addEventListener('click', e => {
+        accordion.classList.toggle('active');
+    })
+})
